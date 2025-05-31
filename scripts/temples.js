2fef39a6d74f571173ccbd4cd6ad2ed8a73d5ecf
methodoti <marcelo@patricio.net.br>
2025-05-31 03:23:59 -0300

    W02: Temple Album fix getdates

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,30 @@ document.addEventListener('DOMContentLoaded', function () {
         menuButton.classList.toggle('open');
     });
 });
+
+
+
+/* getdates.js */
+// select the DOM elements for outpu
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const myname = document.querySelector("#myname");
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = `<span>$ {
+    today.getFullYear()
+}
+
+</span>`;
+
+
+let oLastModif = new Date(document.lastModified);
+
+// lastModified.innerHTML = `<span>${oLastModif.getFullYear()}</span>`;
+lastModified.innerHTML = `<span class="highlight">$ {
+    document.lastModified
+}
+
+</span>`;
