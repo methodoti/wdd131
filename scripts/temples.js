4a6e3060f6f9a256b08a6caf47b715eebf059e8a
methodoti <marcelo@patricio.net.br>
2025-05-31 03:32:44 -0300

    W02: Temple Album last fix on getdate

--- scripts/temples.js
+++ scripts/temples.js
@@ -19,18 +19,12 @@ const myname = document.querySelector("#myname");
 // use the date object
 const today = new Date();
 
-year.innerHTML = `<span>$ {
-    today.getFullYear()
-}
-
-</span>`;
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
 
 
 let oLastModif = new Date(document.lastModified);
-
 // lastModified.innerHTML = `<span>${oLastModif.getFullYear()}</span>`;
-lastModified.innerHTML = `<span class="highlight">$ {
-    document.lastModified
-}
+lastModified.innerHTML = `<span class="highlight">${document.lastModified}</span>`;
 
-</span>`;
+// let omyname = "Marcelo Severo Patricio"
+// myname.innerHTML = `<span class="highlight">${omyname}`;
